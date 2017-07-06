31e9fe94b6fbbb823ff7459563e859b10bef5b2e
Johan Andersson <joha@netcompany.com>
2017-07-06 19:46:49 +0200

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sudo apt update \
-  && sudo apt install git ansible -y
+  && sudo apt install git ansible -y \
+  && cd ansible \
+  && ansible-playbook -b bootstrap.yml -i hosts.yml
