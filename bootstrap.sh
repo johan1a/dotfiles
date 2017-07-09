1006f6216b3c7aee655beab8c7a0b32fa58130d5
Johan Andersson <joha@netcompany.com>
2017-07-09 17:07:06 +0200

    travis

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,4 @@
 
 sudo apt-get update \
   && sudo pip install ansible \
-  && cd ansible \
-  && ansible-playbook bootstrap.yml -i hosts.ini --sudo
+  && ansible-playbook ansible/bootstrap.yml -i ansible/hosts.ini --sudo
