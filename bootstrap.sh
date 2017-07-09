d156fb285cc85b4c2460e7bac506d463ce44e4ce
Johan Andersson <joha@netcompany.com>
2017-07-09 18:27:30 +0200

    Fix syntax

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update \
-  && sudo apt-get install python-pip -y
-  && pip install --upgrade pip
-  && sudo pip install ansible \
+  && sudo apt-get install python-pip -y \
+  && pip install --upgrade pip \
+  && sudo apt-get install ansible=2.0.0.2-2ubuntu1 \
   && ansible-playbook ansible/bootstrap.yml -i ansible/hosts.ini --sudo
+#  && sudo pip install ansible==2.0.0.2 \
