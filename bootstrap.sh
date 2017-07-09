bca41c503f11e7de06cd9c233ba0c211078cc5bb
Johan Andersson <joha@netcompany.com>
2017-07-09 18:14:35 +0200

    add pip to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update \
+  && sudo apt-get install python-pip -y
+  && pip install --upgrade pip
   && sudo pip install ansible \
   && ansible-playbook ansible/bootstrap.yml -i ansible/hosts.ini --sudo
