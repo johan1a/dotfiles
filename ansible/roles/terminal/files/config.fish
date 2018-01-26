6bb4239c012cf42af35bfb771c612e3c44a572c6
Johan Andersson <joha@netcompany.com>
2018-01-26 23:54:39 +0100

    add base16 helper to fish

--- ansible/roles/terminal/files/config.fish
+++ ansible/roles/terminal/files/config.fish
@@ -6,6 +6,11 @@ if status --is-interactive;
     [ -e $HOME/.aliases.sh ]; and . $HOME/.aliases.sh
 end
 
+# Base16 Shell
+if status --is-interactive
+   source $HOME/.config/base16-shell/profile_helper.fish
+end
+
 source ~/.config/fish/functions/utils.fish
 
 alias sf="source ~/.config/fish/config.fish"
