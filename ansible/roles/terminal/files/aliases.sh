13098ecebb6308a60bca788e42b1c2eb41fe1ab9
Johan Andersson <joha@netcompany.com>
2018-03-21 18:26:15 +0100

    sort aliases|

--- ansible/roles/terminal/files/aliases.sh
+++ ansible/roles/terminal/files/aliases.sh
@@ -1,27 +1,6 @@
 
 # Find auto completion:
 # complete -p git
-
-
-
-alias vim=nvim
-alias v=nvim
-
-alias g='git'
-#complete -o bashdefault -o default -o nospace -F _git g
-
-alias ga='git add'
-alias gc='git commit -m '
-alias gd='git diff'
-alias gdd='git diff --cached'
-alias gs='git status -s'
-alias gl='git lg'
-alias gb='git branch -v'
-alias gp='git pull --rebase'
-
-alias amend='git commit --amend'
-alias sdkman-init='source ~/.sdkman/bin/sdkman-init.sh'
-alias ..="cd .."
 alias ..2="cd ../.."
 alias ..3="cd ../../.."
 alias ..4="cd ../../../.."
@@ -30,51 +9,64 @@ alias ..6="cd ../../../../../.."
 alias ..7="cd ../../../../../../.."
 alias ..8="cd ../../../../../../../.."
 alias ..9="cd ../../../../../../../../.."
+alias ..="cd .."
+alias a="ag"
+alias amend='git commit --amend'
+alias cf="vim ~/.config/fish"
+alias ci3="vim ~/.config/.i3/config"
+alias cl=clear
+alias cv="vim ~/.vimrc"
+alias d="docker"
+alias dev="cd ~/dev"
+alias dotfiles="cd ~/dotfiles"
+alias dp="docker stop "
+alias dps="docker ps "
+alias dr="docker restart "
+alias ds="trans da:sv "
+alias dt="docker start "
+alias e=exit
+alias executable="chmod +x "
 alias f="find . -name "
+alias fconfig="cd ~/.config/fish/"
+alias futils="vim ~/.config/fish/functions/utils.fish"
+alias g='git'
+alias ga='git add'
+alias gb='git branch -v'
+alias gc='git commit -m '
+alias gd='git diff'
+alias gdd='git diff --cached'
+alias gl='git lg'
+alias gp='git pull --rebase'
+alias gs='git status -s'
+alias intercept="strace -ff -e trace=write -e write=1,2 -p "
+alias l='ls -alF'
+alias la='ls -A'
+alias less="less -S"
+alias list-jks="keytool -list -v -keystore "
+alias ll='ls -alF'
+alias lt='ls -ltr'
+alias p=pwd
+alias pa='ps ax | a'
+alias package='dpkg -S /usr/bin/ls'
+alias pj="python -m json.tool"
+alias print-keymap="xkbcomp -xkb $DISPLAY my_xkb_keymap"
+alias printcert="keytool -printcert -v -file "
+alias r=rg
+alias rclocal="sudo /etc/rc.local"
+alias readram="sudo dd if=/dev/mem | cat | strings"
+alias s=sudo
+alias sd="trans sv:da "
+alias sdkman-init='source ~/.sdkman/bin/sdkman-init.sh'
+alias serve='python -m SimpleHTTPServer' # optional arg: port (defaults to 8000)
+alias t=cat
 alias ta="tmux attach -t "
 alias td="tmux detach"
 alias tl="tmux list-sessions"
-#alias t="tmux"
 alias trs='tmux rename-session'
-alias a="ag"
-alias pj="python -m json.tool"
-alias serve='python -m SimpleHTTPServer' # optional arg: port (defaults to 8000)
-alias dotfiles="cd ~/dotfiles"
-alias dev="cd ~/dev"
-alias e=exit
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -alF'
-alias lt='ls -ltr'
-alias s=sudo
-alias readram="sudo dd if=/dev/mem | cat | strings"
-alias intercept="strace -ff -e trace=write -e write=1,2 -p "
-alias pa='ps ax | a'
-alias package='dpkg -S /usr/bin/ls'
-alias youtube-mp3='youtube-dl -t --extract-audio --audio-format mp3 '
-alias p=pwd
-alias d="docker"
-alias dps="docker ps "
-alias dt="docker start "
-alias dp="docker stop "
-alias dr="docker restart "
-alias rclocal="sudo /etc/rc.local"
-alias fconfig="cd ~/.config/fish/"
-alias futils="vim ~/.config/fish/functions/utils.fish"
-alias cl=clear
+alias v=nvim
+alias vim=nvim
+alias xc="xclip -selection clipboard "
 alias y=yaourt
+alias youtube-mp3='youtube-dl -t --extract-audio --audio-format mp3 '
 alias ys="yaourt -S "
 alias yss="yaourt -Ss "
-alias less="less -S"
-alias executable="chmod +x "
-alias list-jks="keytool -list -v -keystore "
-alias printcert="keytool -printcert -v -file "
-alias xc="xclip -selection clipboard "
-alias t=cat
-alias ci3="vim ~/.config/.i3/config"
-alias cf="vim ~/.config/fish"
-alias cv="vim ~/.vimrc"
-alias sd="trans sv:da "
-alias ds="trans da:sv "
-alias print-keymap="xkbcomp -xkb $DISPLAY my_xkb_keymap"
-alias r=rg
