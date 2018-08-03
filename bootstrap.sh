bfdda63589380156e2c8c6778931acab3352edbf
Johan Andersson <joha@netcompany.com>
2018-08-03 20:39:29 +0200

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,6 @@ elif [[ "${OS}" == *"Arch Linux"* ]] ; then
   sudo pacman -S python-pip --noconfirm
 fi
 
-pip install ansible --user
 pip install --upgrade --user pip
+pip install ansible
 ansible-playbook ansible/bootstrap.yml -i ansible/hosts.ini --become
