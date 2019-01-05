be2ad84aa8c52727d95370c73837a043769c0f21
Johan Andersson <joha@netcompany.com>
2019-01-05 12:17:52 +0100

    add xdg env vars

--- ansible/roles/terminal/files/config.fish
+++ ansible/roles/terminal/files/config.fish
@@ -6,7 +6,13 @@ set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow -g "!{.git,node_modules
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -x BROWSER google-chrome-stable
 
-
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DESKTOP_DIR="$HOME/"
+export XDG_DOCUMENTS_DIR="$HOME/documents/"
+export XDG_DOWNLOAD_DIR="$HOME/downloads/"
+export XDG_MUSIC_DIR="$HOME/music"
+export XDG_PICTURES_DIR="$HOME/pictures"
+export XDG_VIDEOS_DIR="$HOME/videos"
 
 if status --is-interactive;
     [ -e $HOME/.aliases.sh ]; and . $HOME/.aliases.sh
