0071483bfa9e981d4532d3f0342e1b3ba2ccc227
Johan Andersson <joha@netcompany.com>
2019-02-02 20:30:04 +0100

    fix wrong user being used

--- install.sh
+++ install.sh
@@ -17,12 +17,12 @@ pacstrap /mnt base git sudo
 
 # fstab
 genfstab -U /mnt >> /mnt/etc/fstab
-echo "org /home/$user/dev vboxsf uid=$user,gid=wheel,rw,dmode=700,fmode=600,nofail 0 0" >> /mnt/etc/fstab
+echo "org /home/$username/dev vboxsf uid=$username,gid=wheel,rw,dmode=700,fmode=600,nofail 0 0" >> /mnt/etc/fstab
 
 # chroot
 wget https://gitlab.com/johan1a/dotfiles/raw/master/chroot-install.sh /mnt/chroot-install.sh -O /mnt/chroot-install.sh
 chmod +x /mnt/chroot-install.sh
-arch-chroot /mnt /bin/bash -c "./chroot-install.sh $user $password"
+arch-chroot /mnt /bin/bash -c "./chroot-install.sh $username $password"
 
 # reboot
 umount /mnt
