1b607fb89f722487586b8e2ef8563d443c1dbfac
Johan Andersson <joha@netcompany.com>
2021-01-12 16:54:35 +0100

    try to fix git build

--- tfconfig/project.clj
+++ tfconfig/project.clj
@@ -9,5 +9,6 @@
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
-  :plugins [[lein-cljfmt "0.7.0"]]
+  ; :plugins [[lein-cljfmt "0.7.0"]
+            ]
   )
