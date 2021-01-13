e34c676b819f19dbe229605d7f5a7d7a9580d295
Johan Andersson <joha@netcompany.com>
2021-01-13 15:09:23 +0100

    add debug print

--- tfconfig/src/tfconfig/common/has_executable.clj
+++ tfconfig/src/tfconfig/common/has_executable.clj
@@ -5,6 +5,7 @@
   [executable]
   (println (str "has-executable? " executable))
   (let [result (command "which" [executable] {})
+        _ (println result)
         exit-code (:exit result)]
     (println result)
     (= exit-code 0)))
