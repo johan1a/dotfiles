fcf1a9c2e34e1930bd4224c19c70408cdcb97f49
Johan Andersson <joha@netcompany.com>
2021-01-13 16:16:27 +0100

    stat -> test

--- tfconfig/src/tfconfig/common/file.clj
+++ tfconfig/src/tfconfig/common/file.clj
@@ -5,14 +5,15 @@
   [dir options]
   (let [desired-state (:state options)
         owner (:owner options)
-        is-dir (= 0 (:code (command "stat" ["-d" dir] options)))
-        is-file (= 0 (:code (command "stat" ["-f" dir] options)))
+        is-dir (= 0 (:code (command "test" ["-d" dir] options)))
+        is-file (= 0 (:code (command "test" ["-f" dir] options)))
         ]
     (println "is-dir:" is-dir)
     (println "is-file" is-dir)
-    (when (and (= desired-state "dir") (not is-dir))
-        (println (str "Creating directory: " dir)
-        (command "mkdir" ["-p" dir] (assoc options :sudo true))))
-    (when owner
-      (println (str "Setting owner to: " owner " for directory: " dir)
-      (command "chown" [owner dir] (assoc options :sudo true))))))
+    (do
+      (when (and (= desired-state "dir") (not is-dir))
+          (println (str "Creating directory: " dir)
+          (command "mkdir" ["-p" dir] (assoc options :sudo true))))
+      (when owner
+        (println (str "Setting owner to: " owner " for directory: " dir)
+        (command "chown" [owner dir] (assoc options :sudo true)))))))
