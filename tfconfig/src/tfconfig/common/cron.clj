945fa5d2dccb096645a1b922736e464203813b3a
Johan Andersson <johan.peter.andersson@gmail.com>
2021-01-17 13:57:45 +0100

    touch crontab

--- tfconfig/src/tfconfig/common/cron.clj
+++ tfconfig/src/tfconfig/common/cron.clj
@@ -12,9 +12,11 @@
   [context name job]
   (let [managed-str (str (:managed-str context) name)
         cron-file (<< "/var/spool/cron/~(:username context)")]
-    (with-open [reader (clojure.java.io/reader cron-file)]
-      (let [lines (line-seq reader)
-            index (.indexOf lines managed-str)
-            new-lines (updated-content lines index managed-str job)]
-        (with-open [writer (clojure.java.io/writer cron-file)]
-          (dorun (map #(.write writer (str % "\n")) new-lines)))))))
+    (do
+      (command "touch" [cron-file] context)
+      (with-open [reader (clojure.java.io/reader cron-file)]
+        (let [lines (line-seq reader)
+              index (.indexOf lines managed-str)
+              new-lines (updated-content lines index managed-str job)]
+          (with-open [writer (clojure.java.io/writer cron-file)]
+            (dorun (map #(.write writer (str % "\n")) new-lines))))))))
