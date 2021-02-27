dcbee17b2ec3aab28778db5d4906966d5487e811
Johan Andersson <johan.peter.andersson@gmail.com>
2021-02-27 16:21:40 +0100

    add lein-cljfmt

--- tfconfig/project.clj
+++ tfconfig/project.clj
@@ -10,5 +10,4 @@
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
-  ; :plugins [[lein-cljfmt "0.7.0"] ]
-  )
+  :plugins [[lein-cljfmt "0.7.0"]])
