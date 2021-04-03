7f7aa7db4545220affc1a0152da48d3e19dba5a1
Johan Andersson <johan.peter.andersson@gmail.com>
2021-04-03 17:33:05 +0200

    feat: Make email notifications disappear after 10 sec. Don't notify for sent mails. Use firefox as default browser

--- tfconfig/src/tfconfig/modules/terminal/module.clj
+++ tfconfig/src/tfconfig/modules/terminal/module.clj
@@ -1,20 +1,18 @@
 (ns tfconfig.modules.terminal.module
-  (:require [tfconfig.common.command :refer :all]
-            [tfconfig.common.file :refer :all]
-            [tfconfig.common.pacman :refer :all]
-            [tfconfig.common.has-executable :refer :all]
-            [tfconfig.common.handler :refer :all]
-            [tfconfig.common.aur :refer :all]
+  (:require [tfconfig.common.command :refer [command]]
+            [tfconfig.common.file :refer [link file directory file-exists? dir-exists?]]
+            [tfconfig.common.pacman :refer [pacman]]
+            [tfconfig.common.has-executable :refer [has-executable?]]
+            [tfconfig.common.handler :refer [handler]]
+            [tfconfig.common.aur :refer [install-aur-package]]
             [clojure.core.strint :refer [<<]]))
 
 (defn set-default-shell
   [context changes]
-  (do
-    (command "chsh" ["-s" "/usr/bin/fish"] (assoc context :input (:password context)))))
+    (command "chsh" ["-s" "/usr/bin/fish"] (assoc context :input (:password context))))
 
 (defn link-files
   [context]
-  (do
     (let [src-dir (<< "~(:modules-dir context)terminal/files")
           functions-dir (str src-dir "/functions")
           dest-dir (str (:home context) ".config/fish")]
@@ -23,7 +21,7 @@
       (link context (<< "~{functions-dir}/fish_prompt.fish") (<< "~{dest-dir}/functions/fish_prompt.fish"))
       (link context (<< "~{functions-dir}/utils.fish") (<< "~{dest-dir}/functions/utils.fish"))
       (link context (<< "~{functions-dir}/fish_user_key_bindings.fish") (<< "~{dest-dir}/functions/fish_user_key_bindings.fish"))
-      (link context (<< "~{src-dir}/config.fish") (<< "~{dest-dir}/config.fish")))))
+      (link context (<< "~{src-dir}/config.fish") (<< "~{dest-dir}/config.fish"))))
 
 (defn set-permissions
   [context]
@@ -31,7 +29,6 @@
 
 (defn setup-fish
   [context]
-  (do
     ; (handler context :installed-fish set-default-shell) ; FIXME it does not enter the password correctly, just hangs
     (pacman "fish" (assoc context :state "present"));:handler-ref :installed-fish))
     (link context (<< "~(:modules-dir context)terminal/files/bashrc") (str (:home context) ".bashrc"))
@@ -39,15 +36,14 @@
     (link context (<< "~(:modules-dir context)terminal/files/aliases.sh") (str (:home context) ".aliases.sh"))
     (directory context (<< "~(:home context).config/fish/functions"))
     (link-files context)
-    (set-permissions context)))
+    (set-permissions context))
 
 (defn setup-termite
   [context]
-  (do
     (directory context (<< "~(:home context).config/termite"))
     (link context (<< "~(:modules-dir context)terminal/files/config_termite") (str (:home context) ".config/termite/config"))
     (when-not (has-executable? context "termite")
-      (install-aur-package context "termite-git")))) ; TODO install community/termite instead?
+      (install-aur-package context "termite-git"))) ; TODO install community/termite instead?
 
 (defn setup-base16
   [context]
@@ -58,19 +54,24 @@
 
 (defn setup-gtk3
   [context]
-  (do
     (let [base-dir (<< "~(:home context).config/gtk-3.0")
           src-file (<< "~(:modules-dir context)terminal/files/gtk.css")
           dest-file (str base-dir "/gtk.css")]
       (when-not (file-exists? context dest-file)
         (directory context base-dir)
-        (link context src-file dest-file)))))
+        (link context src-file dest-file))))
+
+(defn setup-defaults
+  [context]
+    (let [dest-file (<< "~(:home context).config/mimeapps.list")
+          src-file (<< "~(:modules-dir context)terminal/files/mimeapps.list")]
+  (link context src-file dest-file)))
 
 (defn run
   [context]
-  (do
-    (setup-fish context)
-    (setup-termite context)
-    (setup-base16 context)
-    (setup-gtk3 context)))
+  (setup-fish context)
+  (setup-termite context)
+  (setup-base16 context)
+  (setup-gtk3 context)
+  (setup-defaults context))
 
