7d3414540cf02cfbc6701a0d4efa51378c268d16
Johan Andersson <johan.peter.andersson@gmail.com>
2021-12-10 23:45:30 +0100

    feat: Use colemak for login screen (vconsole / linux console)

--- tfconfig/src/tfconfig/modules/colemak/module.clj
+++ tfconfig/src/tfconfig/modules/colemak/module.clj
@@ -7,7 +7,10 @@
   [context]
   (do
     (let [src (<< "~(:modules-dir context)colemak/files/colemaknordic")
-          dest "/usr/share/X11/xkb/symbols/colemaknordic"]
+          dest "/usr/share/X11/xkb/symbols/colemaknordic"
+          vconsole-src (<< "~(:modules-dir context)colemak/files/vconsole.conf")
+          vconsole-dest "/etc/vconsole.conf"]
       (link (assoc context :sudo true) src dest)
+      (link (assoc context :sudo true) vconsole-src vconsole-dest)
       (when (:has-monitor (:profile context))
         (command "setxkbmap" ["colemaknordic" "-option" "altwin:swap_lalt_lwin" "-option" "caps:super"] context)))))
