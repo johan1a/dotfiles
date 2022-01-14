9cb6b2b2bd5a6bfaa2c6c55dce48dff1feb343a8
Johan Andersson <johan.peter.andersson@gmail.com>
2022-01-14 22:55:08 +0100

    feat: Add ctrl-n mapping in fish for opening a file in nvim with fzf

--- tfconfig/src/tfconfig/modules/terminal/files/functions/fish_user_key_bindings.fish
+++ tfconfig/src/tfconfig/modules/terminal/files/functions/fish_user_key_bindings.fish
@@ -4,5 +4,6 @@ function fish_user_key_bindings
   bind -M visual V edit_command_buffer
   bind -M insert \cs search-contents
   bind -M insert \cn "s wiki search"
+  bind -M insert \ce "$VISUAL (fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}')"
 end
 
