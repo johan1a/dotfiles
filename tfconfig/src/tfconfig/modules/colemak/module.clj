c478a4f21c537bd00c5c010f02bcabc15e1eb6c3
Johan Andersson <johan.peter.andersson@gmail.com>
2022-05-20 15:51:55 +0200

    feat: set X11 keymap to colemak

--- tfconfig/src/tfconfig/modules/colemak/module.clj
+++ tfconfig/src/tfconfig/modules/colemak/module.clj
@@ -5,11 +5,14 @@
 
 (defn run
   [context]
-  (let [src (<< "~(:modules-dir context)colemak/files/colemaknordic")
-        dest "/usr/share/X11/xkb/symbols/colemaknordic"
+  (let [colemaknordic-src (<< "~(:modules-dir context)colemak/files/colemaknordic")
+        colemaknordic-dest "/usr/share/X11/xkb/symbols/colemaknordic"
         vconsole-src (<< "~(:modules-dir context)colemak/files/vconsole.conf")
-        vconsole-dest "/etc/vconsole.conf"]
-    (link (assoc context :sudo true) src dest)
+        vconsole-dest "/etc/vconsole.conf"
+        xorg-config-src (<< "~(:modules-dir context)colemak/files/00-keyboard.conf")
+        xorg-config-dest "/etc/X11/xorg.conf.d/00-keyboard.conf"]
+    (link (assoc context :sudo true) colemaknordic-src colemaknordic-dest)
     (link (assoc context :sudo true) vconsole-src vconsole-dest)
+    (link (assoc context :sudo true) xorg-config-src xorg-config-dest)
     (when (:has-monitor (:profile context))
       (command "setxkbmap" ["-layout" "us" "-variant" "colemak" "-option" "altwin:swap_lalt_lwin" "-option" "caps:super"] context))))
