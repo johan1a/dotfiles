44d1a44d0aed02c1e2460534899a68d3dc387fda
Johan Andersson <johan.peter.andersson@gmail.com>
2022-06-09 08:38:10 +0200

    fix: make sure git hooks dir exists

--- tfconfig/src/tfconfig/modules/git_hooks/module.clj
+++ tfconfig/src/tfconfig/modules/git_hooks/module.clj
@@ -1,11 +1,13 @@
 (ns tfconfig.modules.git-hooks.module
   (:require
    [clojure.core.strint :refer [<<]]
-   [tfconfig.common.file :refer [link]]))
+   [tfconfig.common.file :refer [link, file]]))
 
 (defn run
   [context]
   (let [files-dir (<< "~(:modules-dir context)git_hooks/files")
         src-file (str files-dir "/commit-msg")
-        dest-file (str (:root-dir context) "/.git/hooks/commit-msg")]
+        dest-dir (str (:root-dir context) "/.git/hooks")
+        dest-file (str dest-dir "/commit-msg")]
+    (file dest-dir (assoc context :state "dir"))
     (link context src-file dest-file)))
