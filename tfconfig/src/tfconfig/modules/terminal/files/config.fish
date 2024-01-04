641ea5071189317ac28bba5ba79fd4055826daf9
Johan Andersson <johan.peter.andersson@gmail.com>
2024-01-04 10:19:33 +0100

    feat: add pnpm fish config

--- tfconfig/src/tfconfig/modules/terminal/files/config.fish
+++ tfconfig/src/tfconfig/modules/terminal/files/config.fish
@@ -46,3 +46,10 @@ if status is-login
     end
 end
 
+
+# pnpm
+set -gx PNPM_HOME "/home/johan/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
