9db09d13ba139b898a6d4abaec1f15fc1fd43db1
Johan Andersson <johan.peter.andersson@gmail.com>
2024-10-10 11:52:40 +0200

    refactor: Don't put parameters in context if it can be avoided

--- tfconfig/src/tfconfig/common/command.clj
+++ tfconfig/src/tfconfig/common/command.clj
@@ -2,18 +2,16 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(def sudo-prompt "Enter SUDO password (WIP): ")
+(def sudo-prompt "Enter SUDO password: ")
 
 (defn log
   [options & args]
   (when (:verbose options) (println (str/join " " args))))
 
 ; from https://stackoverflow.com/a/45293277
-(defn run-proc
-  [options proc-name args stdout-callback stderr-callback]
-  (let [sudo-prompt (:sudo-prompt options)
-        dir (:dir options)
-        input (:input options)
+(defn- run-proc
+  [options proc-name args stdout-callback stderr-callback input]
+  (let [dir (:dir options)
         pbuilder (ProcessBuilder. (into-array String (flatten [proc-name args])))
         env (.environment pbuilder)
         _0 (when sudo-prompt (.put env "SUDO_PROMPT" sudo-prompt))
@@ -43,7 +41,7 @@
           (recur (conj lines line)))))))
 
 (defn err-callback
-  [context sudo-prompt password stderr stdin]
+  [context password stderr stdin]
   (loop [lines []]
     (let [line (.readLine ^java.io.BufferedReader stderr)]
       (if-not line
@@ -53,22 +51,21 @@
           (recur (conj lines line)))))))
 
 (defn pre-auth
-  [options password]
-  (let [new-options (assoc options :input password)
-        stderr-callback (partial err-callback options sudo-prompt password)]
-    (run-proc new-options "sudo" ["-S" "true"] (partial out-callback options) stderr-callback)))
+  [context password]
+  (let [stderr-callback (partial err-callback context password)]
+    (run-proc context "sudo" ["-S" "true"] (partial out-callback context) stderr-callback password)))
 
 (defn command
   [cmd args context & rest]
   (log context cmd args)
   (let [sudo (some #(= % :sudo) rest)
         password (:password context)
-        new-options (assoc context :input (if sudo password nil))
+        input (if sudo password nil)
         new-cmd (if sudo "sudo" cmd)
         new-args (if sudo (concat ["-S" cmd] args) args)]
     (when (:pre-auth context)
       (pre-auth context password))
-    (let [result (run-proc new-options new-cmd new-args (partial out-callback context) (partial err-callback context sudo-prompt password))]
+    (let [result (run-proc context new-cmd new-args (partial out-callback context) (partial err-callback context password) input)]
       (if (or (= 0 (:code result)) (not (:throw-errors context)))
         result
         (throw (ex-info (str "Error: command failed: " cmd " " args) result))))))
