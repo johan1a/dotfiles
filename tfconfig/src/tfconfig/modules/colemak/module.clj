464918cbc74cbf5c5ea67df8f030158e599e5a9f
Johan Andersson <johan.peter.andersson@gmail.com>
2024-10-10 14:42:39 +0200

    fix: colemak module

--- tfconfig/src/tfconfig/modules/colemak/module.clj
+++ tfconfig/src/tfconfig/modules/colemak/module.clj
@@ -11,8 +11,8 @@
         vconsole-dest "/etc/vconsole.conf"
         xorg-config-src (<< "~(:modules-dir context)colemak/files/00-keyboard.conf")
         xorg-config-dest "/etc/X11/xorg.conf.d/00-keyboard.conf"]
-    (link (assoc context :sudo true) colemaknordic-src colemaknordic-dest)
-    (link (assoc context :sudo true) vconsole-src vconsole-dest)
-    (link (assoc context :sudo true) xorg-config-src xorg-config-dest)
+    (link context colemaknordic-src colemaknordic-dest { :sudo true })
+    (link context vconsole-src vconsole-dest { :sudo true })
+    (link context xorg-config-src xorg-config-dest { :sudo true })
     (when (:has-monitor (:profile context))
       (command "setxkbmap" ["-layout" "us" "-variant" "colemak" "-option" "altwin:swap_lalt_lwin" "-option" "ctrl:swapcaps"] context))))
