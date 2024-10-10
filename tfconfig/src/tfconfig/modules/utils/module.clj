04ce2d15f0d9135eb285fd3e00f95c6116e6eb85
Johan Andersson <johan.peter.andersson@gmail.com>
2024-10-10 14:33:17 +0200

    fix: utils module

--- tfconfig/src/tfconfig/modules/utils/module.clj
+++ tfconfig/src/tfconfig/modules/utils/module.clj
@@ -6,26 +6,30 @@
 (defn run
   "Installs useful utility scripts"
   [context]
-  (let [executable-context (assoc context :executable true)
-        home (:home context)
+  (let [home (:home context)
         bin-dir (<< "~(:modules-dir context)utils/files/bin")]
     (doseq [script (.list (file bin-dir))]
       (when script
-        (link executable-context
+        (link context
               (<< "~{bin-dir}/~{script}")
-              (str home ".local/bin/" script))))
-    (link executable-context
+              (str home ".local/bin/" script)
+              {:executable true})))
+    (link context
           (<< "~(:modules-dir context)utils/files/local_scripts_common/s")
-          (str home ".local/bin/s"))
-    (link executable-context
+          (str home ".local/bin/s")
+          {:executable true})
+    (link context
           (<< "~(:modules-dir context)utils/files/local_scripts_common/new-local-script")
-          (str home ".local/bin/new-local-script"))
-    (link executable-context
+          (str home ".local/bin/new-local-script")
+          {:executable true})
+    (link context
           (<< "~(:modules-dir context)utils/files/local_scripts_common/edit-local-script")
-          (str home ".local/bin/edit-local-script"))
-    (link executable-context
+          (str home ".local/bin/edit-local-script")
+          {:executable true})
+    (link context
           (<< "~(:modules-dir context)utils/files/local_scripts_common/list-local-scripts")
-          (str home ".local/bin/list-local-scripts"))
+          (str home ".local/bin/list-local-scripts")
+          {:executable true})
     (directory context (str home ".config/fish/completions"))
     (link context
           (<< "~(:modules-dir context)utils/files/local_scripts_common/s_completions.fish")
