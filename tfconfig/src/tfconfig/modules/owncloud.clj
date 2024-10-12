4811856c2cb5dfd309b71152362f6202260a4eef
Johan Andersson <johan.peter.andersson@gmail.com>
2024-10-12 12:22:32 +0200

    chore: install owncloud package in owncloud module

--- tfconfig/src/tfconfig/modules/owncloud.clj
+++ tfconfig/src/tfconfig/modules/owncloud.clj
@@ -1,9 +1,11 @@
 (ns tfconfig.modules.owncloud
   (:require [tfconfig.common.file :refer [link]]
+            [tfconfig.common.pacman :refer [pacman]]
             [clojure.core.strint :refer [<<]]))
 
 (defn run
   [context]
   (let [home (:home context)]
     (link context (<< "~{home}ownCloud/documents") (<< "~{home}documents"))
-    (link context (<< "~{home}ownCloud/pictures") (<< "~{home}pictures"))))
+    (link context (<< "~{home}ownCloud/pictures") (<< "~{home}pictures"))
+    (pacman "owncloud" context)))
