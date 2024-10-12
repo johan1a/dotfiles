9f879922657a2dd81d7f97448d268d1fc10e0918
Johan Andersson <johan.peter.andersson@gmail.com>
2024-10-12 15:47:15 +0200

    feat: Install jetbrains-toolbox

--- tfconfig/src/tfconfig/modules/intellij/module.clj
+++ tfconfig/src/tfconfig/modules/intellij/module.clj
@@ -1,6 +1,8 @@
 (ns tfconfig.modules.intellij.module
-  (:require [tfconfig.common.file :refer [link]]))
+  (:require [tfconfig.common.file :refer [link]]
+            [tfconfig.common.aur :refer [aur-packages]]))
 
 (defn run
   [context]
-  (link context (str (:modules-dir context) "intellij/files/ideavimrc") (str (:home context) ".ideavimrc")))
+  (link context (str (:modules-dir context) "intellij/files/ideavimrc") (str (:home context) ".ideavimrc"))
+  (aur-packages context "jetbrains-toolbox"))
