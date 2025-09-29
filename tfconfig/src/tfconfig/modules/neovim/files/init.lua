4f82c4dca300ccec73048dca07304a18df9f392e
Johan Andersson <johan.peter.andersson@gmail.com>
2025-09-29 09:10:18 +0200

    feat: Show hidden files in nerdtree

--- tfconfig/src/tfconfig/modules/neovim/files/init.lua
+++ tfconfig/src/tfconfig/modules/neovim/files/init.lua
@@ -314,6 +314,10 @@ vim.api.nvim_set_keymap("n", "<silent> ge", "<Plug>CamelCaseMotion_ge", { silent
 
 vim.g["sneak#label"] = 1
 
+-- =========== nerdtree ===========
+
+vim.g.NERDTreeShowHidden=1
+
 --   ____                           _            _   _   _
 --  / ___| ___ _ __   ___ _ __ __ _| |  ___  ___| |_| |_(_)_ __   __ _ ___
 -- | |  _ / _ \ '_ \ / _ \ '__/ _` | | / __|/ _ \ __| __| | '_ \ / _` / __|
